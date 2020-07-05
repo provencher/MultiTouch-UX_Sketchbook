c6d699678f348646d37486949186e1d4bc794022
Eric <eprovencher92@gmail.com>
2020-07-05 13:07:10 -0400

    Some minor refactoring

---
+++ Assets/UX_Sketchbook/MultiTouch/Scripts/Input/InputSource.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace prvncher.UX_Sketchbook.MultiTouch.Input
+{
+    using Input = UnityEngine.Input;
+
+    public class InputSource : MonoBehaviour
+    {
+        private Vector3 position;
+        private float width;
+        private float height;
+
+        void Awake()
+        {
+            width = (float)Screen.width / 2.0f;
+            height = (float)Screen.height / 2.0f;
+
+            // Position used for the cube.
+            position = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+
+        void OnGUI()
+        {
+            // Compute a fontSize based on the size of the screen width.
+            GUI.skin.label.fontSize = (int)(Screen.width / 25.0f);
+
+            GUI.Label(new Rect(20, 20, width, height * 0.25f),
+                "x = " + position.x.ToString("f2") +
+                ", y = " + position.y.ToString("f2"));
+        }
+
+        void Update()
+        {
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                Touch touch = Input.GetTouch(i);
+
+                Vector2 pos = touch.position;
+                pos.x = (pos.x - width) / width;
+                pos.y = (pos.y - height) / height;
+                position = new Vector3(-pos.x, pos.y, 0.0f);
+
+                Debug.Log($"Input {i} pos: {position} screenPos {pos}");
+            }
+        }
+    }
+}
